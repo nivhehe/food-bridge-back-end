019699ba55edc0d38b05da2dc3b52259ed245cb6
nivhehe <nivedmohan5423@gmail.com>
2025-11-16 17:15:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/food', require('./routes/food'));
 
 // MongoDB & Server
-mongoose.connect('mongodb://127.0.0.1:27017/foodbridge', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(5001, () => console.log('API running on http://localhost:5001')))
+const mongoUri = process.env.MONGODB_URI;
+
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(process.env.PORT || 5001, () =>
+      console.log(`API running on port ${process.env.PORT || 5001}`)))
   .catch(err => console.log('MongoDB connection error', err));
